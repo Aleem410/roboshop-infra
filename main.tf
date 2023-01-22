8b392fe0cae78cef0c17ac49a8d0f64732379422
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-22 19:05:17 +0400

    robo

--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
 resource "aws_spot_instance_request" "ec2" {
-  count  =  length(var.instances)
-  ami  =  data.aws_ami.centos8.id
-  instance_type = "t3.micro"
+  count                  = length(var.instances)
+  ami                    = data.aws_ami.centos8.id
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  tags = {
+  tags                   = {
     Name = element(var.instances, count.index )
   }
-
-
 }
-
 data "aws_ami" "centos8" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
