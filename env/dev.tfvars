72e7eb958a224ee236b2a1003ead6b4b24514c5c
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-22 18:55:24 +0400

    robo

---
+++ env/dev.tfvars
@@ -0,0 +1,7 @@
+instances = [
+  "frontend",
+  "cart",
+  "catalogue",
+  "shipping",
+  "user"
+]
---
+++ main.tf
@@ -0,0 +1,42 @@
+resource "aws_spot_instance_request" "ec2" {
+  count  =  length(var.instances)
+  ami  =  data.aws_ami.centos8.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  tags = {
+    Name = element(var.instances, count.index )
+  }
+
+
+}
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "all"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-00a494e89f26a41c5"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
