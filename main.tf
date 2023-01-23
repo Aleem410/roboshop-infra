ec05e88ce8dfec11e01007f4714c19fd2adad022
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-23 23:49:38 +0400

    robo

--- main.tf
+++ main.tf
@@ -4,10 +4,3 @@ module "vpc" {
   public_cidr_block = var.public_cidr_block
   env = var.env
 }
-
-module "subnets" {
-  source = "github.com/Aleem410/tf-module-vpc"
-  public_cidr_block = var.public_cidr_block
-  vpc_cidr_block = var.vpc_cidr_block
-  env = var.env
-}
