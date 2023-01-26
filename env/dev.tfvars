111f3180cf960665920b2917d2febfd9900d07e4
Abdul Aleem <ma.aleem20@gmail.com>
2023-01-26 21:32:40 +0400

    robo

--- env/dev.tfvars
+++ env/dev.tfvars
@@ -2,4 +2,5 @@ env = "dev"
 
 #vpc
 vpc_cidr_block = "10.0.0.0/16"
-public_cidr_block = "10.0.1.0/24"
+subnet_cidr_block = ["10.0.1.0/2  4", "10.0.2.0/24"]
+subnet_azs = ["us-east-1a", "us-east-1b"]
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "vpc" {
   source = "github.com/Aleem410/tf-module-vpc"
   vpc_cidr_block = var.vpc_cidr_block
-  public_cidr_block = var.public_cidr_block
+  subnet_cidr_block = var.subnet_cidr_block
+  subnet_azs  =  var.subnet_azs
   env = var.env
 }
